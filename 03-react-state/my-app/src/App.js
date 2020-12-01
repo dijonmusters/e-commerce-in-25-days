621ac02fb37b57c5be23ef3d2bf155aaddd5636b
Jon Meyers <jonathon.d.meyers@gmail.com>
2020-12-01 23:12:32 +1100

    add lesson 3

---
+++ 03-react-state/my-app/src/App.js
@@ -0,0 +1,22 @@
+import logo from "./logo.svg";
+import "./App.css";
+import Message from "./components/Message";
+import Button from "./components/Button";
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <Button />
+        <Button />
+        <Button />
+        <Button />
+        <Button />
+        <Button />
+        <Button />
+      </header>
+    </div>
+  );
+}
+
+export default App;
--- README.md
+++ README.md
@@ -1,3 +1,9 @@
 # Build an E-commerce Platform in 25 days with Next.js, Netlify and Stripe
 
-I will update this as we complete the course!
+[Playlist on YouTube](https://www.youtube.com/watch?v=Cn8Y9zmnbDM&list=PLFGFKs8nQ3EbG-ykV8xc1mBt99sqq2Ssx)
+
+Day 1 - Introduction
+
+Day 2 - React Basics: What is a component?
+
+Day 3 - React Basics: What is state?
